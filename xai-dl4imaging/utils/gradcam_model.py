470b74c50cec23b08de5a5c75ae93fee658f0c1e
Christina Bukas <ga27loj@mytum.de>
2022-08-08 17:46:33 +0200

    cleaning up

---
+++ xai-dl4imaging/utils/gradcam_model.py
@@ -0,0 +1,56 @@
+from torchvision.models import resnet50
+import torch.nn as nn
+
+# GradCamModel class with a ResNet pretrained model
+class GradCamModel(nn.Module):
+    def __init__(self):
+        super(GradCamModel, self).__init__()
+        
+        # define the resnet50
+        self.resnet = resnet50(pretrained=True)
+        
+        # isolate the feature blocks
+        self.features = nn.Sequential(self.resnet.conv1,
+                                      self.resnet.bn1,
+                                      nn.ReLU(),
+                                      nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False),
+                                      self.resnet.layer1, 
+                                      self.resnet.layer2, 
+                                      self.resnet.layer3, 
+                                      self.resnet.layer4)
+        
+        # average pooling layer
+        self.avgpool = self.resnet.avgpool
+        
+        # classifier
+        self.classifier = self.resnet.fc
+        
+        # gradient placeholder
+        self.gradient = None
+    
+    # hook for the gradients
+    def activations_hook(self, grad):
+        self.gradient = grad
+    
+    def get_gradient(self):
+        return self.gradient
+    
+    def get_activations(self, x):
+        return self.features(x)
+    
+    def forward(self, x):
+        
+        # extract the features
+        x = self.features(x)
+        
+        # register the hook
+        h = x.register_hook(self.activations_hook)
+        
+        # complete the forward pass
+        x = self.avgpool(x)
+        x = x.view((1, -1))
+        x = self.classifier(x)
+        
+        return x
+
+
