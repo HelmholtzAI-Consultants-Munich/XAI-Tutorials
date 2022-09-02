27dd22f7aab087ca8c8c8c128ab5a2f9c9a48fc9
lisa-sousa <lisa.barros@helmholtz-muenchen.de>
2022-09-02 11:17:45 +0100

    added model training and data processing to FGC notebook

--- xai-FGC/utils/utils.py
+++ xai-FGC/utils/utils.py
@@ -5,7 +5,27 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def plot_feature_importance(importance,names,title):
+def plot_permutation_feature_importance(result, data, title):
+
+    perm_sorted_idx = result.importances_mean.argsort()
+    perm_indices = np.arange(0, len(result.importances_mean)) + 0.5
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
+    fig.suptitle(title)
+    ax1.barh(perm_indices, result.importances_mean[perm_sorted_idx], height=0.7, color = 'lightblue')
+    ax1.set_yticks(perm_indices)
+    ax1.set_yticklabels(data.columns[perm_sorted_idx])
+    ax1.set_ylim((0, len(result.importances_mean)))
+    ax2.boxplot(
+        result.importances[perm_sorted_idx].T,
+        vert=False,
+        labels=data.columns[perm_sorted_idx],
+    )
+    fig.tight_layout()
+    plt.show()
+
+
+def plot_impurity_feature_importance(importance,names,title):
 
     #Create arrays from feature importance and feature names
     feature_importance = np.array(importance)
@@ -30,29 +50,9 @@ def plot_feature_importance(importance,names,title):
 
 def plot_correlation_matrix(data):
 
-    f, ax = plt.subplots(figsize=(5, 4))
+    f, ax = plt.subplots(figsize=(5, 5))
     corr = data.corr()
     mask = np.triu(np.ones_like(corr, dtype=bool))
     np.fill_diagonal(mask, False)
     sns.heatmap(corr, mask=mask, cmap=sns.diverging_palette(220, 10, as_cmap=True), 
     square=True, ax=ax, annot=True)
-
-
-def plot_perm_feature_importance(result, data, title):
-
-    perm_sorted_idx = result.importances_mean.argsort()
-    perm_indices = np.arange(0, len(result.importances_mean)) + 0.5
-
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
-    fig.suptitle(title)
-    ax1.barh(perm_indices, result.importances_mean[perm_sorted_idx], height=0.7, color = 'lightblue')
-    ax1.set_yticks(perm_indices)
-    ax1.set_yticklabels(data.columns[perm_sorted_idx])
-    ax1.set_ylim((0, len(result.importances_mean)))
-    ax2.boxplot(
-        result.importances[perm_sorted_idx].T,
-        vert=False,
-        labels=data.columns[perm_sorted_idx],
-    )
-    fig.tight_layout()
-    plt.show()
