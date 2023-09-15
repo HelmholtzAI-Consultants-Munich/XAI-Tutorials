a4659aaa420f8b289255dc8b1a23817063138edb
donatella-cea <donatella.cea@helmholtz-muenchen.de>
2023-09-15 11:42:52 +0200

    move functions to util, correct typos

---
+++ data_and_models/model_net.py
@@ -0,0 +1,32 @@
+# import torch, torchvision
+# from torchvision import datasets, transforms
+from torch import nn#, optim
+
+
+# Net class with a 2D CNN 
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(1, 10, kernel_size=5),
+            nn.MaxPool2d(2),
+            nn.ReLU(),
+            nn.Conv2d(10, 20, kernel_size=5),
+            nn.Dropout(),
+            nn.MaxPool2d(2),
+            nn.ReLU(),
+        )
+        self.fc_layers = nn.Sequential(
+            nn.Linear(320, 50),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(50, 10),
+            nn.Softmax(dim=1)
+        )
+
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = x.view(-1, 320)
+        x = self.fc_layers(x)
+        return x
--- xai-model-agnostic/utils.py
+++ xai-model-agnostic/utils.py
@@ -7,6 +7,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from torch.nn import functional as F
+import torch, torchvision
+from torchvision import datasets, transforms
+from torch import nn, optim
+
 ############################################################
 ##### Utility Fuctions
 ############################################################
@@ -113,3 +118,58 @@ def plot_correlation_matrix(data):
         ax=ax,
         annot=True,
     )
+
+
+#### Data loader and function for training on MNST###
+
+def train_loader_fun(num_epochs = 5):
+    batch_size = 128
+    #device = torch.device('cpu')
+    return torch.utils.data.DataLoader(
+        datasets.MNIST('mnist_data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor()
+                       ])),
+        batch_size=batch_size, shuffle=True)
+    # return train_loader
+    
+def test_loader_fun(num_epochs = 5):
+    batch_size = 128
+    #device = torch.device('cpu')   
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('mnist_data', train=False, transform=transforms.Compose([
+                           transforms.ToTensor()
+                       ])),
+        batch_size=batch_size, shuffle=True)
+    return test_loader
+    
+def train(model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output.log(), target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 100 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output.log(), target).item() # sum up batch loss
+            pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+    100. * correct / len(test_loader.dataset)))
